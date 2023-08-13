013047ad27a95d8c80afe35797b4187819e5f0f2
Faithful Austin - Ilagha <missfair42@gmail.com>
2023-08-14 00:13:55 +0100

    done with retrieving data on admin

--- app/admin/src/main/java/com/example/admin/TeamActivity.java
+++ app/admin/src/main/java/com/example/admin/TeamActivity.java
@@ -1,25 +1,67 @@
 package com.example.admin;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.admin.Model.Feedback;
+import com.example.admin.Model.Team;
+import com.example.admin.adapter.FeedbackAdapter;
+import com.example.admin.adapter.TeamAdapter;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TeamActivity extends AppCompatActivity {
 
+    private FirebaseFirestore db;
+    private List<Team> teamList;
+    private TeamAdapter adapter;
     RecyclerView teamRecyclerview;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        db=FirebaseFirestore.getInstance();
+        teamList = new ArrayList<>();
+        initList();
+
         setContentView(R.layout.teams);
 
         ImageView backNav = findViewById(R.id.back_nav);
 
         teamRecyclerview = findViewById(R.id.team_recyclerview);
 
+        adapter = new TeamAdapter(this,teamList);
+        teamRecyclerview =findViewById(R.id.team_recyclerview);
+        teamRecyclerview.setLayoutManager(new LinearLayoutManager(this));
+        teamRecyclerview.setAdapter(adapter);
+
         backNav.setOnClickListener(e->{
             finish();
         });
-
     }
+
+    private void initList() {
+        db.collection("teams").get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                QuerySnapshot snapshot = task.getResult();
+                if (!snapshot.isEmpty()) {
+                    for (QueryDocumentSnapshot documentSnapshot : snapshot) {
+                        Team team = documentSnapshot.toObject(Team.class);
+                        teamList.add(team);
+                        Log.d("team",team.getName());
+                        adapter.notifyDataSetChanged();
+                    }
+                }
+            }
+        });
+    }
+
 }
